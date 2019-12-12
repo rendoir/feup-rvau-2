11930713757f1e4dd4dd2febf7d8fdb014bad288
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-12 21:55:15 +0000

    applied layer segregation to line marking

--- src/offside.py
+++ src/offside.py
@@ -31,6 +31,25 @@ def get_points(im, max_points) :
     
     return points
 
+def blend_overlay_with_field(src_img,overlay,transparency):
+    hsv = cv2.cvtColor(src_img, code=cv2.COLOR_BGR2HSV)
+    # green range
+    lower_green = np.array([30, 65, 65])
+    upper_green = np.array([65, 255, 255])
+    # layer masks
+    field_mask = cv2.inRange(hsv, lower_green, upper_green)
+    player_mask = cv2.bitwise_not(field_mask)
+    # player_mask = cv2.morphologyEx(player_mask, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
+    # extract layers from original image
+    field_layer = cv2.bitwise_and(src_img, src_img, mask=field_mask)
+    player_layer = cv2.bitwise_and(src_img, src_img, mask=player_mask)
+    # creates line that is blank where the players are
+    overlay_layer = cv2.bitwise_and(field_layer,overlay)
+    overlay_layer = cv2.GaussianBlur(overlay_layer,(5,5),cv2.BORDER_DEFAULT)
+    field_layer = cv2.addWeighted(field_layer,1,overlay_layer,transparency,0)
+    final = field_layer + player_layer
+    return final
+
 
 if __name__ == '__main__' :
 
@@ -39,6 +58,7 @@ if __name__ == '__main__' :
     # 2008 UEFA Cup Final (Zenit Saint Petersburg vs Rangers) - Manchester City Stadium
     # Origin: Top Right. Field Size: 105m by 68m
     im_dst = cv2.imread('../img/football2.jpg')
+    clean_img = cv2.imread('../img/football2.jpg')
    
     # Create a vector of source points.
     # Real-life Manchester City Stadium - Right Penalty Area Points
@@ -83,12 +103,15 @@ if __name__ == '__main__' :
     line_point_2_im = cv2.perspectiveTransform(line_point_2_rw.reshape(1, 1, -1), h)[0][0]
     #print(line_point_1_im)
     #print(line_point_2_im)
-    
+
     # Draw line
-    overlay_img = im_dst.copy()
+    height, width, channels = clean_img.shape
+    blank_image = np.zeros((height,width,3), np.uint8)
+    overlay_img = blank_image
     cv2.line(overlay_img, tuple(line_point_1_im.astype(int)), tuple(line_point_2_im.astype(int)), (0,0,255), 5, cv2.LINE_AA)
-    cv2.addWeighted(overlay_img, 0.5, im_dst, 0.5, 0, im_dst)
+
+    final = blend_overlay_with_field(clean_img,overlay_img,1.0)
 
     # Display image.
-    cv2.imshow("Image", im_dst)
+    cv2.imshow("Image", final)
     cv2.waitKey(0)
